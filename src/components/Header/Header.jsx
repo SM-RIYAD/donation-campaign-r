668e93f842b223d4fb73d7dae06e09028bab1dad
SM RIYAD <smriyad0185@gmail.com>
2023-09-26 22:26:46 +0600

    finished image bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="flex lg:flex-row flex-col md:flex-col  mx-20 mt-10  lg:mb-0 md:mb-24">
       <div className="flex-grow lg:block md:flex md:justify-center">
-        <img src="../../../images/Logo.png" alt="" />
+        <img src="https://i.ibb.co/QmpXzQn/Logo.png" alt="" />
       </div>
 
       <nav className="flex items-center justify-center z-30 lg:mt-0 mt-10 md:mt-10 gap-10 lg:flex-row flex-col md:flex-col">
